7f8d4d7d278272dba1e57846671056bfab413c44
Taras Chuiko <taraschuiko@gmail.com>
2019-08-06 15:32:21 +0300

    Made note updating

--- client/src/components/Note.vue
+++ client/src/components/Note.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="Note">
-    <h2>{{ note.title }}</h2>
-    <p>{{ note.content }}</p>
+    <div v-if="!this.isEditing">
+      <h2>{{ note.title }}</h2>
+      <p>{{ note.content }}</p>
+    </div>
+    <div v-if="this.isEditing">
+      <h2><input type="text" v-model="newTitle"></h2>
+      <p><input type="text" v-model="newContent"></p>
+    </div>
     <div class="Note__buttons">
-      <button>Edit</button>
+      <button @click="this.edit" v-if="!this.isEditing">Edit</button>
+      <button @click="this.update" v-if="this.isEditing">Update</button>
+      <button @click="this.cancelEdit" v-if="this.isEditing">Cancel</button>
       <button @click="this.delete">Delete</button>
     </div>
   </div>
@@ -12,12 +20,37 @@
 <script>
 export default {
   name: "Note",
+  data() {
+    return {
+      isEditing: false,
+      newTitle: "",
+      newContent: ""
+    };
+  },
   props: {
     note: Object
   },
   methods: {
     delete() {
       this.$store.dispatch("deleteNote", this.note._id);
+    },
+    edit() {
+      this.isEditing = true;
+      this.newTitle = this.note.title;
+      this.newContent = this.note.content;
+    },
+    cancelEdit() {
+      this.isEditing = false;
+      this.newTitle = "";
+      this.newContent = "";
+    },
+    update() {
+      this.isEditing = false;
+      this.$store.dispatch("updateNote", {
+        _id: this.note._id,
+        title: this.newTitle,
+        content: this.newContent
+      })
     }
   }
 };
@@ -30,13 +63,13 @@ export default {
   border-radius: 8px;
   margin-bottom: 6px;
 
-  h2 {
-    font-size: 20px;
+  h2, p {
     margin: 0 0 8px 0;
+    border: 1px solid transparent;
   }
 
-  p {
-    margin: 0 0 8px 0;
+  h2 {
+    font-size: 20px;
   }
 
   &__buttons button {
@@ -44,6 +77,16 @@ export default {
     background: #fff;
     border: 1px solid #ccc;
     border-radius: 4px;
+  }
+
+  input {
+    color: inherit;
+    font-size: inherit;
+    font-family: inherit;
+    font-weight: inherit;
+    padding: 0;
+    border: none;
+    background: #e6e6e6;
   }
 }
 </style>
--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -7,5 +7,8 @@ export default {
   },
   deleteNote(store, id) {
     axios.delete(`${API_URL}/${id}`).then(() => store.dispatch("loadNotes"));
+  },
+  updateNote(store, newNote) {
+    axios.put(`${API_URL}/${newNote._id}`, newNote).then(() => store.dispatch("loadNotes"));
   }
 };
