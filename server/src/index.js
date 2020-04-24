7b2d7839d8d652aff8c1e104350245368f7a5d49
Taras Chuiko <taraschuiko@gmail.com>
2020-04-24 11:46:06 +0300

    Structurized index file

--- server/src/index.js
+++ server/src/index.js
@@ -1,26 +1,31 @@
-const passport = require('koa-passport');
 const Koa = require('koa');
-const cors = require('@koa/cors')
-const bodyParser = require('koa-bodyparser');
-const routes = require('./routes');
-const { setUpConnection, createNote, getNote } = require('./utils/dataBaseUtils');
-const googleStrategy = require('./utils/googleStrategy');
-
 const app = new Koa();
+
+// cors
+const cors = require('@koa/cors')
 app.use(cors());
 
-// app.use(googleStrategy);
-app.use(passport.initialize());
-app.use(passport.session())
-
-if (process.env.NODE_ENV === 'development') require('dotenv').config();
-
-const { PORT, HOST } = process.env;
-
+// db
+const { setUpConnection } = require('./utils/dataBaseUtils');
 setUpConnection();
+
+// bodyparser
+const bodyParser = require('koa-bodyparser');
 app.use(bodyParser());
 
+// routes
+const routes = require('./routes');
 app.use(routes);
+
+// auth
+const passport = require('koa-passport');
+require('./utils/googleStrategy');
+app.use(passport.initialize())
+app.use(passport.session())
+
+// start server
+require('dotenv').config();
+const { PORT, HOST } = process.env;
 app.listen(PORT, () => {
   console.log(`started in http://${HOST}:${PORT}`);
 });
